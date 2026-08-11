ac5c2230e964ad06f283dcd9dea6065d80917f64
adamfoneil <adamfoneil@proton.me>
2026-08-11 15:25:50 -0400

    Add project files.

---
+++ PlainCrud.Abstractions/DbContextExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlainCrud.Abstractions;
+
+public static class DbContextExtensions
+{
+    public static async Task<TKey> InsertAsync<TEntity, TKey>(this DbContext dbContext, TEntity entity, ISqlBuilder sqlBuilder)
+    {
+        var cn = dbContext.Database.GetDbConnection();
+
+        Dictionary<string, object> entityValues = ParseDictionary(entity, dbContext);
+
+        var cmd = sqlBuilder.Insert(entityValues);
+
+        return await cn.QuerySingleAsync<TKey>(cmd);
+    }
+
+    public static async Task UpdateAsync<TEntity, TKey>(this DbContext dbContext, TEntity entity, ISqlBuilder sqlBuilder)
+    {
+        var cn = dbContext.Database.GetDbConnection();
+
+        Dictionary<string, object> entityValues = ParseDictionary(entity, dbContext);
+
+        var cmd = sqlBuilder.Update(entityValues);
+
+        await cn.ExecuteAsync(cmd);        
+    }
+
+    private static Dictionary<string, object> ParseDictionary<TEntity>(TEntity? entity, DbContext dbContext)
+    {
+        ArgumentNullException.ThrowIfNull(entity, nameof(entity));
+
+        var entityType = dbContext.Model.FindEntityType(typeof(TEntity))
+            ?? throw new InvalidOperationException($"Entity type '{typeof(TEntity).Name}' is not registered in the DbContext model.");
+
+        var result = new Dictionary<string, object>();
+
+        foreach (var property in entityType.GetProperties())
+        {
+            var columnName = property.GetColumnName();
+            var value = property.PropertyInfo?.GetValue(entity);
+            result[columnName] = value ?? DBNull.Value;
+        }
+
+        return result;
+    }
+}
---
+++ PlainCrud.Abstractions/ISqlBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Dapper;
+
+namespace PlainCrud.Abstractions;
+
+public interface ISqlBuilder
+{    
+    CommandDefinition Insert(IDictionary<string, object> entity);
+    CommandDefinition Update(IDictionary<string, object> entity);
+}
