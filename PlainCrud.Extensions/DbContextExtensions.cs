dbc349cc375a8e96f046a68627d53b3b92e7350c
adamfoneil <adamfoneil@proton.me>
2026-08-11 16:52:55 -0400

    SaveAsync method

--- PlainCrud.Extensions/DbContextExtensions.cs
+++ PlainCrud.Extensions/DbContextExtensions.cs
@@ -9,18 +9,30 @@ namespace PlainCrud.Abstractions;
 /// </summary>
 public static class DbContextExtensions
 {
-    public static async Task<TKey> InsertAsync<TEntity, TKey>(this DbContext dbContext, TEntity entity, ISqlProvider sqlBuilder)
+    public static async Task<TKey> SaveAsync<TEntity, TKey>(this DbContext dbContext, TEntity entity, Func<TEntity, bool> isInsert, ISqlProvider sqlProvider) where TKey : notnull
+    {
+        if (isInsert(entity))
+        {
+            return await InsertAsync<TEntity, TKey>(dbContext, entity, sqlProvider);
+        }
+
+        await UpdateAsync<TEntity, TKey>(dbContext, entity, sqlProvider);
+
+        return GetKeyValue<TEntity, TKey>(entity, dbContext);
+    }
+
+    public static async Task<TKey> InsertAsync<TEntity, TKey>(this DbContext dbContext, TEntity entity, ISqlProvider sqlProvider) where TKey : notnull
     {
         var cn = dbContext.Database.GetDbConnection();
 
         var (table, idCol, properties) = ParseSqlElements(entity, dbContext);
 
-        var cmd = sqlBuilder.Insert(table, idCol, properties);
+        var cmd = sqlProvider.Insert(table, idCol, properties);
 
         return await cn.QuerySingleAsync<TKey>(cmd);
     }
 
-    public static async Task UpdateAsync<TEntity, TKey>(this DbContext dbContext, TEntity entity, ISqlProvider sqlBuilder)
+    public static async Task UpdateAsync<TEntity, TKey>(this DbContext dbContext, TEntity entity, ISqlProvider sqlBuilder) where TKey : notnull
     {
         var cn = dbContext.Database.GetDbConnection();
 
@@ -29,6 +41,19 @@ public static class DbContextExtensions
         var cmd = sqlBuilder.Update(table, idCol, properties);
 
         await cn.ExecuteAsync(cmd);        
+    }
+
+    private static TKey GetKeyValue<TEntity, TKey>(TEntity entity, DbContext dbContext) where TKey : notnull
+    {
+        var entityType = dbContext.Model.FindEntityType(typeof(TEntity))
+            ?? throw new InvalidOperationException($"Entity type '{typeof(TEntity).Name}' is not registered in the DbContext model.");
+
+        var keyProperty = entityType.FindPrimaryKey()?.Properties.SingleOrDefault()
+            ?? throw new InvalidOperationException($"Entity type '{typeof(TEntity).Name}' must have a single-column primary key.");
+
+        return keyProperty.PropertyInfo?.GetValue(entity) is TKey value
+            ? value
+            : throw new InvalidOperationException($"Key value of entity '{typeof(TEntity).Name}' is null or not of type '{typeof(TKey).Name}'.");
     }
 
     private static (string TableName, string IdentityColumn, Dictionary<string, object> Properties) ParseSqlElements<TEntity>(TEntity? entity, DbContext dbContext)
